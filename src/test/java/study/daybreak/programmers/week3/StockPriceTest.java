3f06b341104777045b39dfd472c5d26b6a691727
LangVE <langve0@gmail.com>
2021-06-22 16:22:19 +0900

    주식가격

---
+++ src/test/java/study/daybreak/programmers/week3/StockPriceTest.java
@@ -0,0 +1,19 @@
+package study.daybreak.programmers.week3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StockPriceTest {
+    @Test
+    public void test_case1() {
+        // given
+        int[] prices = new int[]{1, 2, 3, 2, 3};
+
+        // when
+        StockPrice stockPrice = new StockPrice();
+        int[] actual = stockPrice.solution(prices);
+
+        // then
+        Assert.assertArrayEquals(new int[]{4, 3, 1, 1, 0}, actual);
+    }
+}
