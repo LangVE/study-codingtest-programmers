03a21db1fddb864744fc6dd7fbfb0768e49b8bef
LangVE <langve0@gmail.com>
2021-07-27 01:28:40 +0900

    정수 삼각형

---
+++ src/test/java/study/daybreak/programmers/week7/IntegerTriangleTest.java
@@ -0,0 +1,20 @@
+package study.daybreak.programmers.week7;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegerTriangleTest {
+
+    @Test
+    public void solution_case1() {
+        // given
+        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+
+        // when
+        IntegerTriangle integerTriangle = new IntegerTriangle();
+        int actual = integerTriangle.solution(triangle);
+
+        // then
+        Assert.assertEquals(30, actual);
+    }
+}
